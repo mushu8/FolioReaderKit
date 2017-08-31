eb218abeb2ca5592515edb6b448058d901db13ee
Alexandre Sagette <alsagette@airfrance.fr>
2017-08-31 15:15:16 +0200

    clear scrolviw background color

--- Source/FolioReaderPage.swift
+++ Source/FolioReaderPage.swift
@@ -54,7 +54,6 @@ open class FolioReaderPage: UICollectionViewCell, UIWebViewDelegate, UIGestureRe
             webView.dataDetectorTypes = .link
             webView.scrollView.showsVerticalScrollIndicator = false
             webView.scrollView.showsHorizontalScrollIndicator = false
-			webView.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1) //UIColor.clear
             
             self.contentView.addSubview(webView)
         }
